9d84416be72305eb8f5b1a9864e89b1162b47c22
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2024-07-31 21:32:46 +0530

    msg

--- config/passport-config.js
+++ config/passport-config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `https://hossom.shop/auth/google/callback`
+  callbackURL: `https://hossom.onrender.com/auth/google/callback`
 },
   async (accessToken, refreshToken, profile, done) => {
     try {
